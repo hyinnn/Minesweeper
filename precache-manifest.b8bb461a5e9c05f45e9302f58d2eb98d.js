27774ee4fbb187b08e6b9bb1e9c4b9a32b5aa8cb
hyinnn <henry.yin@uq.net.au>
2020-04-03 15:47:48 -0700

    Updates

---
+++ precache-manifest.b8bb461a5e9c05f45e9302f58d2eb98d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93ca5b28a28c7ecee309ce77dc0e3039",
+    "url": "/minesweeper/index.html"
+  },
+  {
+    "revision": "04ce35c2ddf46d28bba8",
+    "url": "/minesweeper/static/css/main.8721bec7.chunk.css"
+  },
+  {
+    "revision": "51bcf9af9abc7ebeb809",
+    "url": "/minesweeper/static/js/2.32848ea8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/minesweeper/static/js/2.32848ea8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04ce35c2ddf46d28bba8",
+    "url": "/minesweeper/static/js/main.cbcb3b92.chunk.js"
+  },
+  {
+    "revision": "df4575315e1e3f1edb44",
+    "url": "/minesweeper/static/js/runtime-main.e1c02939.js"
+  }
+]);
